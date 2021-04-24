a829b2bc219daa39f570707885d40bb9033e5f99
iylinkim <kiylin94@naver.com>
2021-04-24 16:02:29 +0900

    Install node-sass

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import "./scss/App.scss";
 
 function App() {
   return (
